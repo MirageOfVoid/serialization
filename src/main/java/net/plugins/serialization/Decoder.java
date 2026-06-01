4c9479af1449743e47436f39431933a05ed97e78
MirageOfVoid <3e_esin_roman@bk.ru>
2026-06-01 12:19:43 +0300

    new codec

--- src/main/java/net/plugins/serialization/Decoder.java
+++ src/main/java/net/plugins/serialization/Decoder.java
@@ -1,17 +1,30 @@
 package net.plugins.serialization;
 
 import com.google.gson.JsonElement;
+import net.plugins.serialization.ops.DynamicOps;
 
 import java.util.function.Function;
 
 public interface Decoder<R> {
-    DataResult<R> decode(JsonElement element);
+    <T> DataResult<R> decode(DynamicOps<T> ops, T t);
 
     default <T> Decoder<T> map(Function<R, T> mapper) {
-        return element -> Decoder.this.decode(element).map(mapper);
+//        return element -> Decoder.this.decode(element).map(mapper);
+        return new Decoder<T>() {
+            @Override
+            public <T1> DataResult<T> decode(DynamicOps<T1> ops, T1 t1) {
+                return Decoder.this.decode(ops, t1).map(mapper);
+            }
+        };
     }
 
     default <T> Decoder<T> flatMap(Function<R, DataResult<T>> mapper) {
-        return element -> decode(element).flatMap(mapper);
+//        return element -> decode(element).flatMap(mapper);
+        return new Decoder<T>() {
+            @Override
+            public <T1> DataResult<T> decode(DynamicOps<T1> ops, T1 t1) {
+                return Decoder.this.decode(ops, t1).flatMap(mapper);
+            }
+        };
     }
 }
--- src/main/java/net/plugins/serialization/Encoder.java
+++ src/main/java/net/plugins/serialization/Encoder.java
@@ -1,17 +1,30 @@
 package net.plugins.serialization;
 
 import com.google.gson.JsonElement;
+import net.plugins.serialization.ops.DynamicOps;
 
 import java.util.function.Function;
 
 public interface Encoder<R> {
-    DataResult<JsonElement> encode(R input);
+    <T> DataResult<T> encode(DynamicOps<T> ops, R input);
 
     default <T> Encoder<T> comap(Function<T, R> mapper) {
-        return input -> encode(mapper.apply(input));
+//        return input -> encode(mapper.apply(input));
+        return new Encoder<T>() {
+            @Override
+            public <T1> DataResult<T1> encode(DynamicOps<T1> ops, T input) {
+                return Encoder.this.encode(ops, mapper.apply(input));
+            }
+        };
     }
 
     default <T> Encoder<T> flatComap(Function<T, DataResult<R>> mapper) {
-        return input -> mapper.apply(input).flatMap(this::encode);
+//        return input -> mapper.apply(input).flatMap(this::encode);
+        return new Encoder<T>() {
+            @Override
+            public <T1> DataResult<T1> encode(DynamicOps<T1> ops, T input) {
+                return mapper.apply(input).flatMap(r -> Encoder.this.encode(ops, r));
+            }
+        };
     }
 }
